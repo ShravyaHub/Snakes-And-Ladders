6a6387396dbe368ffb8f05b2c0c049eedb4f6595
ShravyaHubb <shravyak146@gmail.com>
2020-10-24 21:33:10 +0530

    Modified file: StartGame.java, added code to play game with two players

--- StartGame.java
+++ StartGame.java
@@ -4,45 +4,106 @@ public class StartGame {
 
     public static final int START_POSITION = 0;
     public static final int WINNING_POSITION = 100;
+    public static final int NO_PLAY = 1;
     public static final int LADDER = 2;
     public static final int SNAKE = 3;
 
     public static void main(String[] args) {
 
-        int playerPosition = START_POSITION, dieRoll, options, numberOfDieRolls = 0;
+        boolean player1IsPlaying = true;
+        int player1Position = START_POSITION, player2Position = START_POSITION, dieRoll, options, dieRolledTimes = 0;
         Random random = new Random();
 
-        while(playerPosition < WINNING_POSITION) {
+        while(player1Position != WINNING_POSITION && player2Position != WINNING_POSITION) {
 
             dieRoll = random.nextInt(6) + 1;
-            numberOfDieRolls++;
+            dieRolledTimes++;
             options = random.nextInt(3) + 1;
 
-            if (options == LADDER) {
+            if (options == NO_PLAY) {
 
-                if (playerPosition + dieRoll <= WINNING_POSITION) {
+                if(player1IsPlaying) {
 
-                    playerPosition += dieRoll;
-                    System.out.println("Player landed on a ladder");
+                    player1Position += 0;
+                    player1IsPlaying = false;
 
                 } else {
 
-                    System.out.println("Player needs " + (WINNING_POSITION - playerPosition) + " to win!");
+                    player2Position += 0;
+                    player1IsPlaying = true;
+
+                }
+
+            } else if (options == LADDER) {
+
+                if(player1IsPlaying) {
+
+                    if (player1Position + dieRoll <= WINNING_POSITION) {
+
+                        player1Position += dieRoll;
+                        System.out.println("Player 1 landed on a ladder");
+
+                    }
+
+                } else {
+
+                    if (player2Position + dieRoll <= WINNING_POSITION) {
+
+                        player2Position += dieRoll;
+                        System.out.println("Player 2 landed on a ladder");
+
+                    }
+
                 }
 
             } else if (options == SNAKE) {
 
-                playerPosition -= dieRoll;
-                if(playerPosition < START_POSITION) {
-                    playerPosition = START_POSITION;
+                if(player1IsPlaying) {
+
+                    player1Position -= dieRoll;
+                    player1IsPlaying = false;
+
+                    if (player1Position < 0) {
+
+                        player1Position = 0;
+
+                    }
+
+                    System.out.println("Player 1 landed on a snake");
+
+                } else {
+
+                    player2Position -= dieRoll;
+                    player1IsPlaying = true;
+
+                    if (player2Position < 0) {
+
+                        player2Position = 0;
+
+                    }
+
+                    System.out.println("Player 2 landed on a snake");
+
                 }
-                System.out.println("Player landed on a snake");
 
             }
-            System.out.println("Player position: " + playerPosition);
+
+            System.out.println("Player 1 position  = " + player1Position);
+            System.out.println("Player 2 position  = " + player2Position);
+
         }
 
-        System.out.println("The die was rolled " + numberOfDieRolls + " to win the game");
+        if(player1Position == 100) {
+
+            System.out.println("Player 1 won the game!");
+
+        } else {
+
+            System.out.println("Player 2 won the game!");
+
+        }
+
+        System.out.println("Dice was rolled " + dieRolledTimes + " to win the game");
 
     }
 

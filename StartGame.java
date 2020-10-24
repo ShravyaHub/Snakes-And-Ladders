aff611f8819c531de1932edb8c99ec4c04862e53
ShravyaHubb <shravyak146@gmail.com>
2020-10-24 20:57:13 +0530

    Modified file: StartGame.java, added code to check if player landed on ladder or snake or remains in same position

--- StartGame.java
+++ StartGame.java
@@ -3,12 +3,27 @@ import java.util.Random;
 public class StartGame {
 
     public static final int START_POSITION = 0;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
 
     public static void main(String[] args) {
 
-        int playerPosition = START_POSITION;
+        int playerPosition = START_POSITION, dieRoll, options;
         Random random = new Random();
-        int dieRoll = random.nextInt(6) + 1;
+        dieRoll = random.nextInt(6) + 1;
+        options = random.nextInt(3) + 1;
+
+        if (options == LADDER) {
+
+            playerPosition += dieRoll;
+            System.out.println("Player landed on a ladder");
+
+        } else if (options == SNAKE){
+
+            playerPosition -= dieRoll;
+            System.out.println("Player landed on a snake");
+
+        }
 
     }
 
